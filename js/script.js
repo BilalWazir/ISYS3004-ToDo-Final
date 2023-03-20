2c5f1a1e947e33578b998c8057d290da796b5e00
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-03-20 09:46:36 +0000

    updated clicking on box

--- js/script.js
+++ js/script.js
@@ -15,6 +15,17 @@ function onAddTaskClicked(event) {
     todoListContainer.insertAdjacentHTML('afterbegin', taskHTML);
 }
 
+function onTodoListContainerClicked(event) {
+    while (!targetElement.classList.contains("task")){
+        targetElement = targetElement.parentElement;
+    }
+    var checkbox = targetElement.querySelector(".checkbox");
+    if (checkbox.checked) {
+        targetElement.classList.add("completed")
+    } else {
+        targetElement.classList.remove("completed")
+    }
+}
 
 //Step 3, Link to event handler
 
