9108ce16b3df0a2bf329e9100bbb2144cf165bb5
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-19 16:40:12 +0000

    changes to JS

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
-// Step 1 , find the element
+let addTaskButton = document.getElementById("add-task");
+let newTaskInput = document.getElementById("task-input");
+let todoListContainer = document.getElementById("todo-list");
 
-var addTaskButton = document.getElementById("add-task");
-var newTaskInput = document.getElementById("task-input");
-var todoListContainer = document.getElementById("todo-list");
-var templateContainer = document.getElementById("list-item-template");
-var template = templateContainer.innerHTML;
+let templateElement = document.getElementById("list-item-template");
 
-//Step 2, Write the behaviour
+let template = templateElement.innerHTML;
+
 
 function onAddTaskClicked(event) {
+
     let taskName = newTaskInput.value;
+
     newTaskInput.value = "";
+
     let taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
+
+
     todoListContainer.insertAdjacentHTML('beforeend', taskHTML);
 }
 
 
 function onTodolistClicked(event) {
+
     let targetElement = event.target;
+
+
     while (!targetElement.classList.contains("task")) {
         targetElement = targetElement.parentElement;
     }
 
+  
     let checkbox = targetElement.querySelector(".checkbox");
+
+
+ 
     if (checkbox.checked) {
         targetElement.classList.add("completed");
     } else {
@@ -30,8 +41,7 @@ function onTodolistClicked(event) {
     }
 }
 
-//Step 3, Link to event handler
+
 
 addTaskButton.addEventListener('click', onAddTaskClicked);
 todoListContainer.addEventListener('click', onTodolistClicked);
-
