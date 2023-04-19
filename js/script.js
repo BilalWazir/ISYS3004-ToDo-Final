b64229daebde6d7b62ae1978fc9cde3dbfeca567
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-19 17:31:12 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -1,39 +1,27 @@
 let addTaskButton = document.getElementById("add-task");
 let newTaskInput = document.getElementById("task-input");
 let todoListContainer = document.getElementById("todo-list");
-
 let templateElement = document.getElementById("list-item-template");
-
 let template = templateElement.innerHTML;
 
-
 function onAddTaskClicked(event) {
-
     let taskName = newTaskInput.value;
-
+    /* Now clear the text box */
     newTaskInput.value = "";
 
     let taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
 
-
     todoListContainer.insertAdjacentHTML('beforeend', taskHTML);
+
 }
 
-
 function onTodolistClicked(event) {
-
     let targetElement = event.target;
-
 
     while (!targetElement.classList.contains("task")) {
         targetElement = targetElement.parentElement;
     }
-
-  
     let checkbox = targetElement.querySelector(".checkbox");
-
-
- 
     if (checkbox.checked) {
         targetElement.classList.add("completed");
     } else {
@@ -41,7 +29,7 @@ function onTodolistClicked(event) {
     }
 }
 
-
-
+/* Step 3 make the event trigger our functions
+-----------------------------------------------*/ 
 addTaskButton.addEventListener('click', onAddTaskClicked);
 todoListContainer.addEventListener('click', onTodolistClicked);
